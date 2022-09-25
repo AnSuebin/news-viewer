e8889628bf46d4cb9cb8ec051447e0b3f8905486
SooBin An <jejupalette@naver.com>
2022-09-25 17:37:39 +0900

    feat : 비동기적으로 뉴스 정보 api 통해 가져오기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=fba7a3abc501445ab9a18643f2f5104a'
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
